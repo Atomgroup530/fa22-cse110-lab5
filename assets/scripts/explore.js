63ce4a22da168b304c9b9afc2b1155df145a1f10
Atomgroup530 <xul007@ucsd.edu>
2022-10-27 16:47:23 -0700

    #1 load SpeechSynthesizer and populate the “Select Voice” dropdown

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,33 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // initiate constants
+  const textArea = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const button = document.getElementsByClassName('button')[0];
+  // #1 load SpeechSynthesizer and populate the “Select Voice” dropdown
+  const synth = window.speechSynthesis;
+  let voices = [];
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  // #2 Enable text-to-speech
+  
 }
