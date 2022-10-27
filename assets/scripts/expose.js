db2085e6e7aaa17c6c14703b69186249e508a37a
Atomgroup530 <xul007@ucsd.edu>
2022-10-27 12:48:48 -0700

    Horn - display image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,23 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  // #1 display image and set audio
+  let horn_select = document.getElementById('horn-select');
+  horn_select.addEventListener('change', () => {
+    // display image
+    let image_select = document.getElementsByTagName('img');
+    if (horn_select.value == 'air-horn'){
+      image_select[0].src = 'assets/images/air-horn.svg';
+    }
+    if (horn_select.value == 'car-horn'){
+      image_select[0].src = 'assets/images/car-horn.svg';
+    }
+    if (horn_select.value == 'party-horn'){
+      image_select[0].src = 'assets/images/party-horn.svg';
+    }
+    // 
+  });
+
+  
 }
+
