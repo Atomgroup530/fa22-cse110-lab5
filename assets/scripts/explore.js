356c1d5190ca49f8520727ac234b50ef5b03b932
Atomgroup530 <xul007@ucsd.edu>
2022-10-27 17:32:56 -0700

    #2 Enable text-to-speech

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,9 +4,10 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // initiate constants
-  const textArea = document.getElementById('text-to-speak');
+  const inputTxt = document.getElementById('text-to-speak');
   const voiceSelect = document.getElementById('voice-select');
-  const button = document.getElementsByClassName('button')[0];
+  const button = document.getElementsByTagName('button')[0];
+  const face = document.getElementsByTagName('img')[0];
   // #1 load SpeechSynthesizer and populate the “Select Voice” dropdown
   const synth = window.speechSynthesis;
   let voices = [];
@@ -31,5 +32,38 @@ function init() {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
   // #2 Enable text-to-speech
-  
+  function speak() {
+    if (synth.speaking) {
+      console.error("speechSynthesis.speaking");
+      return;
+    }
+
+    if (inputTxt.value !== '') {
+      const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+
+      utterThis.onend = function (event) {
+        console.log("SpeechSynthesisUtterance.onend");
+      };
+
+      utterThis.onerror = function (event) {
+        console.error("SpeechSynthesisUtterance.onerror");
+      };
+
+      const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+      for (let i = 0; i < voices.length; i++) {
+        if (voices[i].name === selectedOption) {
+          utterThis.voice = voices[i];
+          break;
+        }
+      }
+      synth.speak(utterThis);
+    }
+  }
+
+  button.addEventListener('click', () => {
+    speak();
+  });
+
 }
